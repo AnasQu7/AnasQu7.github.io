16ba3f3fcd8f0bbb1a7fccf133842744dfb99bf2
Mohd Anas Quraishi <techwiser@gmail.com>
2025-10-15 11:11:26 +0530

    fixes

--- src/App.js
+++ src/App.js
@@ -13,36 +13,7 @@ function App() {
   // const navigate = useNavigate()
   const { toggleTheme, Theme } = useContext(ThemeContext);
 
- const chatSettings = {
-    agentId: "68eee4d2cef4f88cc3562938",
-    voice: "Charon",
-    onRedirect : (url) => {
-      // navigate(url)
-      if(url === "/THEME"){
-        toggleTheme()
-        return
-      }
-      console.log(url)
-      let urlArr = url.split(" ")
-      if(urlArr.length > 1){
-        url = urlArr.join("-").toLowerCase()
-      }
-      console.log("the url is ", url)
-      scroller.scrollTo(url, {
-        duration: 800,
-        delay: 0,
-        smooth: 'easeInOutQuart',
-        offset: -100
-      })
-    }
-  };
- const darkChatSettings = {
-    agentId: "68eee4d2cef4f88cc3562938",
-    primaryColor: "#2198bc",
-    secondaryColor: "#123C69",
-    backgroundColor: "#06101c",
-    voice: "Charon",
-    onRedirect : (url) => {
+  const handleRedirect = (url) => {
       // navigate(url)
        if(url === "/THEME"){
         toggleTheme()
@@ -61,6 +32,19 @@ function App() {
         offset: -100
       })
     }
+
+ const chatSettings = {
+    agentId: "68eee4d2cef4f88cc3562938",
+    voice: "Charon",
+    onRedirect : handleRedirect
+  };
+ const darkChatSettings = {
+    agentId: "68eee4d2cef4f88cc3562938",
+    primaryColor: "#2198bc",
+    secondaryColor: "#123C69",
+    backgroundColor: "#06101c",
+    voice: "Charon",
+    onRedirect : handleRedirect
   };
   return (
     <div className="App">
